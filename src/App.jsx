c72e6323856e5a4754c2f63365dce3c97e9b3edf
Radivoje Katanic <k.rade313@gmail.com>
2022-08-18 20:57:48 +0200

    Make sidenav responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import CheckboxAndRadio from "./components/CheckboxAndRadio";
@@ -15,76 +14,70 @@ import SideOverlay from "./components/SideOverlay";
 import Stats from "./components/Stats";
 import Table from "./components/Table";
 
-const App = () => {
-  return (
-    <>
-      <div
-        className={`App overflow-hidden relative dark:bg-zinc-900 py-8 bg-white`}
-      >
-        <div className="flex flex-col gap-16">
-          <Header />
-          <Preview withoutPadding component={<Navbar />} title="Dark navbar" />
-          <Preview alignInCenter component={<Input />} title="Input field" />
-          <Preview alignInCenter component={<Button />} title="Buttons" />
-          <Preview
-            alignInCenter
-            component={<FileUpload />}
-            title="File upload"
-          />
-          <Preview alignInCenter component={<Select />} title="Select" />
-          <Preview
-            alignInCenter
-            component={<CheckboxAndRadio />}
-            title="Checkbox & radio"
-          />
-          <Preview
-            component={<Sidenav />}
-            withoutPadding
-            title="Sidenav with actions and item list"
-          />
-          <Preview
-            alignInCenter
-            component={<Divider />}
-            title="Divider with text"
-          />
-          <Preview
-            alignInCenter
-            component={<Modal />}
-            title="Simple delete confirmation modal"
-          />
-          <Preview alignInCenter component={<Alert />} title="Alerts" />
-          <Preview
-            withoutPadding
-            component={<SideOverlay />}
-            title="Side overlay"
-          />
-          <Preview component={<Table />} title="Table" />
-          <Preview component={<Stats />} title="Stats overview cards" />
-        </div>
+const App = () => (
+  <>
+    <div
+      className={`App overflow-hidden relative dark:bg-zinc-900 py-8 bg-white`}
+    >
+      <div className="flex flex-col gap-16">
+        <Header />
+        <Preview withoutPadding component={<Navbar />} title="Dark navbar" />
+        <Preview alignInCenter component={<Input />} title="Input field" />
+        <Preview alignInCenter component={<Button />} title="Buttons" />
+        <Preview alignInCenter component={<FileUpload />} title="File upload" />
+        <Preview alignInCenter component={<Select />} title="Select" />
+        <Preview
+          alignInCenter
+          component={<CheckboxAndRadio />}
+          title="Checkbox & radio"
+        />
+        <Preview
+          component={<Sidenav />}
+          withoutPadding
+          title="Sidenav with actions and item list"
+        />
+        <Preview
+          alignInCenter
+          component={<Divider />}
+          title="Divider with text"
+        />
+        <Preview
+          alignInCenter
+          component={<Modal />}
+          title="Simple delete confirmation modal"
+        />
+        <Preview alignInCenter component={<Alert />} title="Alerts" />
+        <Preview
+          withoutPadding
+          component={<SideOverlay />}
+          title="Side overlay"
+        />
+        <Preview component={<Table />} title="Table" />
+        <Preview component={<Stats />} title="Stats overview cards" />
       </div>
-      <div className="fixed inset-0 [mask-image:linear-gradient(to_bottom_right,white,transparent,white)]">
-        <svg className="h-full w-full absolute inset-0">
-          <defs>
-            <pattern
-              id="grid"
-              width="40"
-              height="40"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect width="40" height="40" fill="url(#tenthGrid)" />
-              <path
-                d="M 100 0 L 0 0 0 100"
-                fill="none"
-                className="stroke-zinc-200 dark:stroke-zinc-800"
-                stroke-width="1"
-              />
-            </pattern>
-          </defs>
-          <rect width="100%" height="100%" fill="url(#grid)" />
-        </svg>
-      </div>
-    </>
-  );
-};
+    </div>
+    <div className="fixed inset-0 [mask-image:linear-gradient(to_bottom_right,white,transparent,white)]">
+      <svg className="h-full w-full absolute inset-0">
+        <defs>
+          <pattern
+            id="grid"
+            width="40"
+            height="40"
+            patternUnits="userSpaceOnUse"
+          >
+            <rect width="40" height="40" fill="url(#tenthGrid)" />
+            <path
+              d="M 100 0 L 0 0 0 100"
+              fill="none"
+              className="stroke-zinc-200 dark:stroke-zinc-800"
+              stroke-width="1"
+            />
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#grid)" />
+      </svg>
+    </div>
+  </>
+);
 
 export default App;
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   FiEdit2,
   FiEye,
@@ -5,42 +6,95 @@ import {
   FiXCircle,
   FiTrash2,
   FiSave,
+  FiFolder,
+  FiX,
+  FiMenu,
 } from "react-icons/fi";
 import { VscColorMode } from "react-icons/vsc";
 
 const Sidenav = (): JSX.Element => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showFiles, setShowFiles] = useState(false);
+
+  const handleShowMenuoggle = (): void => {
+    setShowMenu((prevState) => !prevState);
+  };
+
+  const handleShowFilesToggle = (): void => {
+    setShowFiles((prevState) => !prevState);
+  };
+
   return (
-    <div className="flex bg-zinc-50 dark:bg-zinc-900/50 dark:bg-opacity-40">
-      <div className="w-32 bg-gradient-to-b from-emerald-600 to-teal-600 flex flex-col items-center gap-12">
-        <div className="flex items-center justify-center h-16 bg-emerald-700/70 w-full">
+    <div className="flex-col flex bg-zinc-50 dark:bg-zinc-900/50 dark:bg-opacity-40 sm:flex-row h-full">
+      {(showMenu || showFiles) && (
+        <div className="lg:hidden fixed inset-0 w-full h-full bg-zinc-900/70 z-100"></div>
+      )}
+      <div className="w-full sm:max-w-[5rem] bg-gradient-to-b from-emerald-600 to-teal-600 flex flex-col items-center gap-6 sm:z-10">
+        <div className="px-8 justify-between flex items-center sm:justify-center sm:px-0 h-16 bg-emerald-700 w-full z-10">
           <div className="w-8 h-8 border-emerald-50 border-8 rounded-full"></div>
+          {showMenu ? (
+            <FiX
+              onClick={handleShowMenuoggle}
+              size="1.375rem"
+              className="stroke-white sm:hidden"
+            />
+          ) : (
+            <FiMenu
+              onClick={handleShowMenuoggle}
+              size="1.375rem"
+              className="stroke-white sm:hidden"
+            />
+          )}
         </div>
-        <div className="flex flex-col items-center gap-4 w-full px-4">
-          <div className="hover:cursor-pointer bg-opacity-50 flex items-center justify-center py-3 rounded-md bg-emerald-700 w-full relative before:rounded-r-md before:-left-7 before:-top-2 before:content-[''] before:h-12 before:w-1.5 before:bg-emerald-50 before:absolute">
+        <div
+          className={`${
+            showMenu ? "top-0" : "-top-full"
+          } z-0 transition-[top] duration-300 absolute sm:relative sm:mt-0 sm:py-0 sm:top-0 mt-16 py-4 bg-gradient-to-b from-emerald-600 to-teal-600  flex flex-col items-center gap-4 w-full px-4`}
+        >
+          <div className="hover:cursor-pointer bg-opacity-50 flex gap-4 items-center justify-center py-3 rounded-md bg-emerald-700 w-full">
             <FiEdit2 size="1.375rem" className="stroke-emerald-100" />
+            <span className="text-white text-lg sm:hidden">Editor</span>
           </div>
-          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer  flex items-center justify-center py-3 rounded-md w-full relative before:rounded-r-md before:-left-7 before:-top-2 before:content-[''] before:h-12 before:w-1.5 before:bg-emerald-50 before:absolute">
+          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer  flex gap-4 items-center justify-center py-3 rounded-md w-full">
             <FiEye size="1.375rem" className="stroke-emerald-100" />
+            <span className="text-white text-lg sm:hidden">Preview</span>
           </div>
-          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex items-center justify-center py-3 rounded-md w-full relative before:rounded-r-md before:-left-7 before:-top-2 before:content-[''] before:h-12 before:w-1.5 before:bg-emerald-50 before:absolute">
+          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex gap-4 items-center justify-center py-3 rounded-md w-full">
             <FiDownload size="1.375rem" className="stroke-emerald-100" />
+            <span className="text-white text-lg sm:hidden">Download</span>
           </div>
-          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex items-center justify-center py-3 rounded-md w-full relative before:rounded-r-md before:-left-7 before:-top-2 before:content-[''] before:h-12 before:w-1.5 before:bg-emerald-50 before:absolute">
+          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex gap-4 items-center justify-center py-3 rounded-md w-full">
             <FiXCircle size="1.375rem" className="stroke-emerald-100" />
+            <span className="text-white text-lg sm:hidden">Clear</span>
+          </div>
+          <div
+            onClick={handleShowFilesToggle}
+            className="lg:hidden hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex gap-4 items-center justify-center py-3 rounded-md w-full"
+          >
+            <FiFolder size="1.375rem" className="stroke-emerald-100" />
+            <span className="text-white text-lg sm:hidden">Files</span>
           </div>
           <div className="bg-emerald-500 h-px w-8"></div>
-          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex items-center justify-center py-3 rounded-md w-full relative before:rounded-r-md before:-left-7 before:-top-2 before:content-[''] before:h-12 before:w-1.5 before:bg-emerald-50 before:absolute">
+          <div className="hover:bg-opacity-50 hover:bg-emerald-700 hover:cursor-pointer flex gap-4 items-center justify-center py-3 rounded-md w-full">
             <VscColorMode size="1.375rem" className="fill-emerald-100" />
+            <span className="text-white text-lg sm:hidden">Theme</span>
           </div>
         </div>
       </div>
 
-      <div className="shadow-sm border-r bg-white pb-32 dark:bg-zinc-800 dark:border-zinc-700 w-full max-w-sm">
-        <div className="border-b h-16 flex items-center px-8 text-lg font-semibold dark:text-zinc-200 dark:border-zinc-700">
-          Notes{" "}
-          <span className="text-sm text-zinc-400 font-normal ml-2">
-            4 files
-          </span>
+      <div
+        className={`${
+          showFiles ? "left-0 sm:ml-20" : ""
+        } z-10 h-full transition-[left] duration-300 absolute -left-full lg:relative lg:left-0 lg:ml-0 shadow-sm border-r bg-white dark:bg-zinc-800 dark:border-zinc-700 w-full max-w-sm  pb-32`}
+      >
+        <div className="border-b h-16 flex items-center justify-between px-8 text-lg font-semibold dark:text-zinc-200 dark:border-zinc-700">
+          <div>
+            Notes{" "}
+            <span className="text-sm text-zinc-400 font-normal ml-2">
+              4 files
+            </span>
+          </div>
+          <FiX onClick={handleShowFilesToggle} className="lg:hidden" />
         </div>
         <div className="w-full max-w-sm">
           <div className="border-b px-8 py-4 bg-zinc-50 dark:bg-zinc-700/25 dark:border-zinc-700">
@@ -98,14 +152,14 @@ const Sidenav = (): JSX.Element => {
         </div>
       </div>
       <div className="w-full">
-        <div className="bg-white w-full border-b h-16 flex items-center justify-center px-8 text-lg font-semibold dark:text-zinc-200 dark:border-zinc-700 dark:bg-zinc-800">
-          <div className="flex items-center justify-between  w-full max-w-5xl">
+        <div className="bg-white w-full border-b h-16 flex items-center justify-center text-lg font-semibold dark:text-zinc-200 dark:border-zinc-700 dark:bg-zinc-800">
+          <div className="flex items-center justify-between w-full max-w-5xl px-8 ">
             <div>Note one</div>
             <FiSave className="dark:stroke-zinc-400 cursor-pointer hover:stroke-emerald-500 dark:hover:stroke-emerald-500" />
           </div>
         </div>
 
-        <div className="h-full w-full max-w-5xl px-8 pt-8">
+        <div className="h-full w-full max-w-5xl m-auto mt-8 px-8">
           <div className="shadow-lg bg-white h-full p-8 rounded-md dark:bg-zinc-800 dark:border-zinc-700">
             <h2 className="font-bold text-2xl mb-2 dark:text-zinc-200">
               <span className="text-zinc-300 dark:text-zinc-600">##</span> Note
